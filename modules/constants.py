9762b7f3a2ee9e1d597194191e3a551ff22ee0f7
Yaroslav Drozdovskyy <drozdovskyj@gmail.com>
2024-10-07 01:00:30 +0200

    Add api urls, user name

--- modules/constants.py
+++ modules/constants.py
@@ -5,3 +5,6 @@ class Data:
     EMAIL = "226f885c-b36c-47bb-a427-5f688f978c5c@mailslurp.net"
     PASSWORD = "User2024*"
     USER_ID = 2983
+    USER_API_URL = "https://greencity-user.greencity.cx.ua"
+    APPLICATION_API_URL = "https://greencity.greencity.cx.ua"
+    USER_NAME = "TestUser"
