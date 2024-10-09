f4435d1c8d78814e3a338e0f2d34bfa3a3ea6c78
Denys Smielov <smelov.denis.2006@gmail.com>
2024-10-09 14:16:00 +0200

    Refactor BasePageGreenCity class to remove inheritance from BasePage

--- ui/pages/green_city/base_page_green_city.py
+++ ui/pages/green_city/base_page_green_city.py
@@ -1,11 +1,10 @@
 from playwright.sync_api import Page
 from ui.components.green_city_header_component import GreenCityHeaderComponent
-from ui.pages.base_page import BasePage
 
 
-class BasePageGreenCity(BasePage):
+class BasePageGreenCity:
     def __init__(self, page: Page):
-        super().__init__(page)
+        self.page = page
 
     def get_header_component(self):
         return GreenCityHeaderComponent(self.page, self.header_root_element)
