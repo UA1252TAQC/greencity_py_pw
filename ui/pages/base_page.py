5a887df5a0d7bf78ea00957bd60cae481d75f9c9
s.bekberov <s.bekberov@gmail.com>
2024-10-10 00:22:10 +0300

    Refactored method "find with wait"

--- ui/pages/base_page.py
+++ ui/pages/base_page.py
@@ -1,18 +1,12 @@
 from playwright.sync_api import Page
 
 
-class BasePage():
-    TIME_TO_WAIT = 15000
+class BasePage:
+    TIME_TO_WAIT = 10000
 
     def __init__(self, page: Page):
         self.page = page
-        self.header_root_element = self.find_with_wait_element("//header[@role='banner']", self.TIME_TO_WAIT)
-
-    def get_pop_up_message(self):
-        return self.find_with_wait_element("//div[@matsnackbarlabel]", self.TIME_TO_WAIT).inner_text()
-
-    def open_url(self, url: str):
-        self.page.goto(url)
+        self.pop_up_message = self.page.wait_for_selector("//div[@matsnackbarlabel]", timeout=BasePage.TIME_TO_WAIT).inner_text()
 
     def open_url_in_new_tab(self, url: str):
         self.page.evaluate(f"window.open('{url}', '_blank');")
@@ -28,6 +22,3 @@ class BasePage():
 
     def get_auth_token(self):
         return self.get_local_storage_item("accessToken")
-
-    def get_current_url(self):
-        return self.page.url
