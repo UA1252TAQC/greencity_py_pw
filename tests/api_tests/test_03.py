93eb1552a1de99c28911618022c2db6a51944bfd
ol271176 <ol271176@gmail.com>
2024-10-10 11:15:35 +0300

    fix delete test method

--- tests/api_tests/test_03.py
+++ tests/api_tests/test_03.py
@@ -22,6 +22,8 @@ def test_delete_shopping_list_item(delete_shopping_list_item, get_first_shopping
     habit_id = get_first_habit_id
     shopping_list_item_id = get_first_shopping_list_item_id
 
-    delete_shopping_list_item(habit_id, shopping_list_item_id)
+    response = delete_shopping_list_item(habit_id, shopping_list_item_id)
+
+    assert response.status_code == 200
 
     log.info(f"Shopping list item with ID {shopping_list_item_id} for habit ID {habit_id} successfully deleted.")
