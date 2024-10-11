bdfc08ffb3b1c8d94bbc4a5129d5dadf750fa331
ol271176 <ol271176@gmail.com>
2024-10-11 10:31:27 +0300

    login refactoring

--- ui/components/login_modal_component.py
+++ ui/components/login_modal_component.py
@@ -7,29 +7,37 @@ from ui.pages.green_city.profile_page import ProfilePage
 
 class LoginModalComponent:
     def __init__(self, page: Page):
+        """
+        Initialize the LoginModalComponent with necessary page elements.
+        """
         self.page = page
         self.email = EmailField(page)
         self.password = PasswordField(page)
-        self.sign_in_button = page.locator("button[type='submit']")
+        self.sign_in_button = page.locator(".sign-in-form button[type='submit']")
 
-    def enter_email(self, email: str):
+    def login(self, email: str, password: str):
+        """
+        Enter email, password, and click the sign-in button.
+        :param email: Email address to be entered.
+        :param password: Password to be entered.
+        """
         self.email.enter(email)
-        return self
-
-    def enter_password(self, password: str):
         self.password.enter(password)
-        return self
-
-    def fill_form(self, email: str, password: str):
-        self.enter_email(email)
-        self.enter_password(password)
+        self.click_sign_in_button()
         return self
 
     def click_sign_in_button(self):
+        """
+        Wait for the sign-in button to appear and click it.
+        """
+        self.page.wait_for_selector("button[type='submit']")
         self.sign_in_button.click()
         return self
 
-    def click_sign_in_button_and_successful_login(self):
+    def click_sign_in_button_and_wait_for_success(self):
+        """
+        Click the sign-in button and wait until it disappears (indicating successful login).
+        """
         self.click_sign_in_button()
         self.sign_in_button.wait_for(state='hidden')
-        return ProfilePage(self.page)
+        return self
