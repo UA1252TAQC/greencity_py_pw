cda4ea3723845de1306f3df8509e9d8338dfe686
oksanafilipchuk <filipchuk.oksana@gmail.com>
2024-10-13 20:31:55 +0300

    add faker and create news data generator

---
+++ modules/fake_data_generator.py
@@ -0,0 +1,17 @@
+from faker import Faker
+import random
+from typing import List
+
+from ui.enum.news_tags import NewsTags
+
+fake = Faker()
+
+def generate_news_data():
+    title = fake.sentence(nb_words=10)
+    content = fake.paragraph(nb_sentences=100)
+    tags = generate_random_tags()
+    return title, content, tags
+
+def generate_random_tags() -> List[NewsTags]:
+    return  random.sample(list(NewsTags), random.randint(1, len(NewsTags)))
+
