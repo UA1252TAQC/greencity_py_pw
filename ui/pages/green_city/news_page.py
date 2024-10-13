eb57ed0656c37e5588ad682678b3585f221fb01d
oksanafilipchuk <filipchuk.oksana@gmail.com>
2024-10-13 20:29:19 +0300

    fix news page locator, import

--- ui/pages/green_city/news_page.py
+++ ui/pages/green_city/news_page.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page
 
-from ui.pages.green_city.create_news_page import CreateNewsPage
+
 from ui.pages.green_city.green_city_base_page import GreenCityBasePage
 
 
@@ -11,9 +11,10 @@ class NewsPage(GreenCityBasePage):
 
     def click_create_news(self, page):
         self.create_news_button.click()
+        from ui.pages.green_city.create_news_page import CreateNewsPage
         return CreateNewsPage(self.page)
 
 
 class NewsPageLocators:
-    CREATE_NEWS_BUTTON = "a[contains(@class,'create')]//div[@id='create-button']"
+    CREATE_NEWS_BUTTON = "//a[contains(@class,'create')]//div[@id='create-button']"
     NEWS_ITEM_BOX = "app-news-list-gallery-view"
