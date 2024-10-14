2618ce5b53af66f44030cc11703cd930a9d49c41
Ponomarov Oleh <ol271176@gmail.com>
2024-10-14 09:47:05 +0300

    add faker

--- tests/ui_tests/login_form_tests/test_128_invalid_password.py
+++ tests/ui_tests/login_form_tests/test_128_invalid_password.py
@@ -1,6 +1,10 @@
 import pytest
+from faker.contrib.pytest.plugin import faker
 
 from modules.constants import Data
+from faker import Faker
+
+fake = Faker()
 
 INVALID_PASSWORD_ERROR_UA = "Введено невірний email або пароль."
 INVALID_PASSWORD_ERROR_EN = "Bad email or password."
@@ -9,24 +13,28 @@ INVALID_PASSWORD_ERROR_EN = "Bad email or password."
 @pytest.mark.parametrize(
     "language, email, password, expected",
     [
-        ("Ua", Data.USER_EMAIL, Data.USER_PASSWORD + "@123", INVALID_PASSWORD_ERROR_UA),
-        ("En", Data.USER_EMAIL, Data.USER_PASSWORD + "@123", INVALID_PASSWORD_ERROR_EN),
+        ("Ua",
+         Data.USER_EMAIL,
+         INVALID_PASSWORD_ERROR_UA),
+        ("En",
+         Data.USER_EMAIL,
+         INVALID_PASSWORD_ERROR_EN),
     ],
     indirect=["setup_function"]
 )
 def test_verify_error_message_for_invalid_password(email,
-                                                   password,
                                                    expected,
                                                    setup_function):
     """
-        This test checks the correctness of error messages when entering an invalid password
-        on login pages with different language settings (Ukrainian and English).
+    This test checks the correctness of error messages when entering a randomly generated invalid password
+    on login pages with different language settings (Ukrainian and English).
 
-        Parameters:
-        - email: The user's email.
-        - password: The incorrect password to test.
-        - expected: The expected error message.
-        """
+    Parameters:
+    - email: The user's email.
+    - expected: The expected error message for invalid password.
+    """
+    password = fake.password(length=12, special_chars=True, digits=True, upper_case=True, lower_case=True)
+
     login_form = setup_function
 
     actual = (login_form
