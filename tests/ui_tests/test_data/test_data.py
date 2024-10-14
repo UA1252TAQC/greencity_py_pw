77e57435a5d3b94b3bc685e8e31f05effa8d2c3d
oksanafilipchuk <filipchuk.oksana@gmail.com>
2024-10-14 11:39:21 +0300

    add data provider

---
+++ tests/ui_tests/test_data/test_data.py
@@ -0,0 +1,9 @@
+from ui.enum.news_tags import NewsTags
+
+test_data = {
+    "news_data_valid": {
+        "title": "Title 1",
+        "content": "Title 1 Title 1 Title 1 Title 1 Title 1 Title 1 Title 1",
+        "tags": [NewsTags.NEWS, NewsTags.EVENTS]
+    }
+}
