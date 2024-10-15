941bd4a3a7be57248e7bd3d327151e943af0c0c4
Vladyslava <vldnezhnova@gmail.com>
2024-10-15 15:08:05 +0200

    Fix file with flake8

--- tests/ui_tests/test_data/test_data.py
+++ tests/ui_tests/test_data/test_data.py
@@ -11,15 +11,15 @@ test_data = {
         "content": "Workshop to educate your customers about eco-friendly living",
         "tags": [NewsTags.EVENTS]
     },
-    "title_length":{
-        "title": "Celebrating World Water Day: The Coral Triangle. Water benefits not only humans but all animals and natural life that exists above and below the surface. Water benefits!!",
+    "title_length": {
+        "title": "Celebrating World Water Day: The Coral Triangle. Water benefits not only humans but all animals "
+                 "and natural life that exists above and below the surface. Water benefits!!",
         "content": "Celebrating World Water Day: The Coral Triangle",
         "tags": [NewsTags.EVENTS]
     },
-"author_field":{
+    "author_field": {
         "title": "Coffee takeaway with 40% discount",
         "content": "It's so healthy, fun and cool to bring eco habits into everyday life",
         "tags": [NewsTags.EVENTS]
     }
 }
-
