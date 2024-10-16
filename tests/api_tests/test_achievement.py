2136a694be38e044fada107194e37789a5273d0e
FOleks <alexsasha878794@gmail.com>
2024-10-16 16:09:27 +0300

    added test_unauthorized_error_in_achievements and fixed test_achievements_by_type_and_category

--- tests/api_tests/test_achievement.py
+++ tests/api_tests/test_achievement.py
@@ -9,25 +9,25 @@ import inspect
 import allure
 
 
-@allure.description("Verify successful receiving of all achievements by type and category")
+@allure.description("Verify successful receiving of all achievements by type \"{achievement_status}\" "
+                    "and category \"{achievement_category_id}\"")
 @allure.feature("Achievements")
 @allure.severity(allure.severity_level.NORMAL)
-@pytest.mark.parametrize("achievementStatus, achievementCategoryId", [("ACHIEVED", 1), ("UNACHIEVED", 2)])
-def test_achievements_by_type_and_category(tc_logger, get_auth_token, achievementStatus, achievementCategoryId):
+@pytest.mark.parametrize("achievement_status, achievement_category_id", [("ACHIEVED", 1), ("UNACHIEVED", 2)])
+def test_achievements_by_type_and_category(tc_logger, get_auth_token, achievement_status, achievement_category_id):
     tc_logger.log_test_name(
-        "Verify successful receiving of all achievements by type and category."
+        "Verify achievements by type and category."
     )
-    api = BaseApi(f'{Data.BASE_URL}/achievements')
+    api = BaseApi(f'{Data.API_BASE_URL}/achievements')
 
-    query_params = {"achievementStatus": achievementStatus,
-                    "achievementCategoryId": achievementCategoryId}
+    query_params = {"achievementStatus": achievement_status,
+                    "achievementCategoryId": achievement_category_id}
 
     headers = {
         'accept': '*/*',
         'Authorization': f'Bearer {get_auth_token}',
         'Content-Type': 'application/json'
     }
-    log.info(f"Test is started: {inspect.currentframe().f_code.co_name}")
     response = api.get_data(headers=headers, query_params=query_params)
     log.info(f"Request URL is: {response.url}")
     log.info(f"Received status code: {response.status_code}")
@@ -45,4 +45,35 @@ def test_achievements_by_type_and_category(tc_logger, get_auth_token, achievemen
     for expected_achievement_category in expected_achievement_categories:
         assert expected_achievement_category in achievement_categories, (
             f'There is no {expected_achievement_category} in response body')
+    log.info(f"Test is finished: {inspect.currentframe().f_code.co_name}")
+
+
+@allure.description("Verify, that unauthorized error is received after request to 'achievements' with invalid token")
+@allure.feature("Achievements")
+@allure.severity(allure.severity_level.NORMAL)
+def test_unauthorized_error_in_achievements(tc_logger):
+    tc_logger.log_test_name(
+        "Verify unauthorized error with invalid token in 'achievements'"
+    )
+    api = BaseApi(f'{Data.API_BASE_URL}/achievements')
+
+    query_params = {"achievementStatus": "ACHIEVED",
+                    "achievementCategoryId": 1}
+
+    headers = {
+        'accept': '*/*',
+        'Authorization': 'Bearer abcdefd123456"',
+        'Content-Type': 'application/json'
+    }
+    response = api.get_data(headers=headers, query_params=query_params)
+    log.info(f"Request URL is: {response.url}")
+    log.info(f"Received status code: {response.status_code}")
+    json_response = response.json()
+    print(type(json_response))
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert json_response.get("error") == "Unauthorized"
+    assert len(json_response) == 4
+    expected_keys = ['timestamp', 'status', 'error', 'path']
+    for expected_key in expected_keys:
+        assert expected_key in response.json(), f'There is no {expected_key} in response body'
     log.info(f"Test is finished: {inspect.currentframe().f_code.co_name}")
