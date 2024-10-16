e6405f4c3616e7bbbd92c1f229faf8e2aad93959
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-10-16 22:52:10 +0300

    136 test_verify_invalid_email_warning

--- tests/ui_tests/test_utils/test_data_loader_csv.py
+++ tests/ui_tests/test_utils/test_data_loader_csv.py
@@ -27,3 +27,18 @@ def get_test_data_by_test_name(filename, test_name):
                 resolved_row = {key: resolve_data_constant(value) for key, value in row.items() if key != 'test_name'}
                 test_data.append(resolved_row)
     return test_data
+
+def get_test_data(filename, test_name):
+    """
+    Loads test data from a CSV file and dynamically builds a list of tuples
+    excluding the 'test_name' field. Replaces string references to constants in the Data class.
+    Returns a list of tuples, where each tuple contains data for a single test case.
+    """
+    test_data = []
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['test_name'] == test_name:
+                resolved_row = tuple(resolve_data_constant(value) for key, value in row.items() if key != 'test_name')
+                test_data.append(resolved_row)
+    return test_data
